7394934610ec1e436c726f886762f4945e1d19e5
shakhyahalder <68652584+shakhya-h@users.noreply.github.com>
2026-02-18 23:14:24 +0530

    Update products.js

--- products.js
+++ products.js
@@ -237,7 +237,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     // 🚨 CONFIG: Backend URL
     // const API_BASE = "http://localhost:8080/api/public"; 
-    const API_BASE = "https://cms-backend-wj8q.onrender.com";
+    const API_BASE = "https://cms-backend-wj8q.onrender.com/api";
     fetchAndRenderCatalog();
 
     async function fetchAndRenderCatalog() {
